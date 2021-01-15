7c9c7741a10c35c90c9d22ee48838460c84f22df
Leonid Shamis <lshamis@thirdwave.ai>
2021-01-15 12:57:03 -0800

    next attempt

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@ from setuptools import setup, Extension
 import glob
 # import subprocess
 
+
+class get_pybind_include(object):
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
+
+
 # subprocess.check_call(['git', 'submodule', 'init'], cwd='./alephzero')
 # subprocess.check_call(['git', 'submodule', 'update'], cwd='./alephzero')
 module = Extension('alephzero_bindings',
                    sources=['module.cc'] + glob.glob('./alephzero/src/*.c*'),
-                   include_dirs = ['./alephzero/include/'],
+                   include_dirs = ['./alephzero/include/', get_pybind_include(), get_pybind_include(user=True)],
                    extra_compile_args = ['-std=c++17', '-O3'])
 
 
