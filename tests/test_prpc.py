d30b8d7c0e8ebc45599d7a25ad3db2ee2c46a31e
Leonid Shamis <leonid.shamis@gmail.com>
2021-09-15 21:12:38 -0700

    Add prpc test

---
+++ tests/test_prpc.py
@@ -0,0 +1,40 @@
+import a0
+import os
+import threading
+
+
+def test_prpc():
+    os.putenv("A0_TOPIC_TMPL_PRPC", "alephzero/{topic}.prpc.a0")
+    a0.File.remove("alephzero/foo.prpc.a0")
+    assert not os.path.exists("/dev/shm/alephzero/foo.prpc.a0")
+
+    cv = threading.Condition()
+    class State:
+        connects = []
+        replies = []
+        done = False
+
+    def onconnect(conn):
+        State.connects.append(conn.pkt.payload)
+        conn.send("progress", False)
+        conn.send("progress", False)
+        conn.send("progress", False)
+        conn.send("progress", False)
+        conn.send("progress", True)
+
+    server = a0.PrpcServer("foo", onconnect, lambda id: None)
+    assert os.path.exists("/dev/shm/alephzero/foo.prpc.a0")
+
+    def onprogress(pkt, done):
+        State.replies.append(pkt.payload)
+        if done:
+            with cv:
+                State.done = True
+                cv.notify()
+
+    client = a0.PrpcClient("foo")
+    client.connect("connect", onprogress)
+
+    with cv:
+        cv.wait_for(lambda: State.done)
+        assert len(State.connects) == 1 and len(State.replies) == 5
